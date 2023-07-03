76676f03bebc94a5467458859631d8b69f2fac2b
LYB9736 <chulfry1222@gmail.com>
2023-07-03 14:59:50 +0900

    고양이

--- ch06/S03.java
+++ ch06/S03.java
@@ -1,71 +1,76 @@
-package ch06;
+// package ch06;
 
-class Cat {
-    String name;
-    int hp;
-    int position;
+// class Cat {
+//     String name;
+//     int hp;
+//     int position;
 
-    public Cat(String name, int hp, int position) {
-        this.name = name;
-        this.hp = hp;
-        this.position = position;
-    }
+//     public Cat(String name, int hp, int position) {
+//         this.name = name;
+//         this.hp = hp;
+//         this.position = position;
+//     }
 
-    public Cat(String name) {
-        this(name, 100, 0);
+//     public Cat(String name) {
+//         this(name, 100, 0);
 
-    }
+//     }
 
-    public void moveRight() {
-        this.position++;
-        System.out.println(this.name + "가 오른쪽으로 이동했습니다.");
+//     public void moveRight() {
+//         this.position++;
+//         System.out.println(this.name + "가 오른쪽으로 이동했습니다.");
 
-        reduceHp();
-    }
+//         reduceHp();
+//     }
 
-    public void moveLeft() {
-        if (this.position < 1) {
-            System.out.println(this.name + "은(는) 왼쪽으로 갈 수 없습니다.");
-            // 함수(메소드)에서 return을 만나면 아래 코드는 실행되지 않는다.
-            return;
-        }
-        this.position--;
-        System.out.println(this.name + "가 왼쪽으로 이동했습니다.");
+//     public void moveLeft() {
+//         if (this.position < 1) {
+//             System.out.println(this.name + "은(는) 왼쪽으로 갈 수 없습니다.");
+//             // 함수(메소드)에서 return을 만나면 아래 코드는 실행되지 않는다.
+//             return;
+//         }
+//         this.position--;
+//         System.out.println(this.name + "가 왼쪽으로 이동했습니다.");
 
-        reduceHp();
-    }
+//         reduceHp();
+//     }
 
-    public void reduceHp() {
-        this.hp--;
-        System.out.println(this.name + "의 체력이 내려갔습니다.");
-    }
+//     public void reduceHp() {
+//         this.hp--;
+//         System.out.println(this.name + "의 체력이 내려갔습니다.");
+//     }
 
-    public void yam(int food){
-        this.hp = this.hp + food;
-        System.out.println(this.name + "가 밥을 먹었습니다.");
+//     public void yam(int food){
+//         if(this.hp + food > 100){
+//             this.hp = 100;
+//             System.out.println("체력이 가득차서 더 이상 먹을 수 없습니다.");
+//             return;
+//         }
+//         this.hp = this.hp + food;
+//         System.out.println(this.name + "가 밥을 먹었습니다.");
 
-    }
+//     }
 
-    public String dong(){
-        return "1557";
-    }
-}
+//     public String dong(){
+//         return "뿌직";
+//     }
+// }
 
-public class S03 {
-    public static void main(String[] args) {
+// public class S03 {
+//     public static void main(String[] args) {
 
-        Cat cat = new Cat("야옹이");
+//         Cat cat = new Cat("야옹이");
 
-        cat.moveLeft();
+//         cat.moveLeft();
 
-        cat.moveRight();
+//         cat.moveRight();
 
-        cat.yam(5);
+//         cat.yam(5);
 
-        System.out.println(cat.hp);
+//         System.out.println(cat.hp);
 
-        System.out.println(cat.dong());
+//         System.out.println(cat.dong());
 
-    }
+//     }
 
-}
+// }
