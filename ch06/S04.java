1fb7ca7218738ec30bb177e0e2a8541ca85aa094
LYB9736 <chulfry1222@gmail.com>
2023-07-04 10:39:57 +0900

    생성자

--- ch06/S04.java
+++ ch06/S04.java
@@ -1,32 +1,32 @@
-package ch06;
+// package ch06;
 
-class Cat{
-    static int countCat = 0;
-    String name;
+// class Cat{
+//     static int countCat = 0;
+//     String name;
 
-    public Cat(){
-        name = "길고양이";
-        Cat.addCat();
-    }
+//     public Cat(){
+//         name = "길고양이";
+//         Cat.addCat();
+//     }
 
-    public static void addCat(){
-        Cat.countCat++;
-        System.out.println("새로운 고양이의 탄생");
-    }
+//     public static void addCat(){
+//         Cat.countCat++;
+//         System.out.println("새로운 고양이의 탄생");
+//     }
 
     
-}
+// }
 
-public class S04 {
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
-        Cat cat3 = new Cat();
-        Cat cat4 = new Cat();
+// public class S04 {
+//     public static void main(String[] args) {
+//         Cat cat = new Cat();
+//         Cat cat1 = new Cat();
+//         Cat cat2 = new Cat();
+//         Cat cat3 = new Cat();
+//         Cat cat4 = new Cat();
         
 
-        System.out.println("고양이는 현재 " + Cat.countCat + "마리 있습니다.");
-    }
+//         System.out.println("고양이는 현재 " + Cat.countCat + "마리 있습니다.");
+//     }
     
-}
+// }
---
+++ ch06/S09.java
@@ -0,0 +1,23 @@
+// package ch06;
+
+// class Cat{
+//     String name;
+
+//     public Cat(String name){
+//         this.name = name;
+//     }
+
+//     public Cat(){
+//         // this()는 첫째줄에 작성해야 한다.
+//         String name = "야옹이";
+//         this(name);
+        
+//     }
+// }
+
+// public class S09 {
+//     public static void main(String[] args) {
+        
+//     }
+    
+// }
