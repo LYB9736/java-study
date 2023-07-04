21e5922cf614e2b5ac9333e0800c1f45b69bb8e9
LYB9736 <chulfry1222@gmail.com>
2023-07-04 17:15:24 +0900

    제어자

--- ch07/sub/Sub01.java
+++ ch07/sub/Sub01.java
@@ -7,6 +7,8 @@ public class Sub01 {
 
     // 객체의 데이터를 외부에 노출 시키는 것은
     // 보통 메소드로 한다.
+    // 원본 데이터를 넘길 수도 있고
+    // 숨겨야 하는 부분은 숨길 수도 있다.
     public String getData(){
         return "숨긴" + data;
     }
