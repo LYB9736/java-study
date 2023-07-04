6d202f6ac2b7905038b3d6349cdc2274508f4c10
LYB9736 <chulfry1222@gmail.com>
2023-07-04 16:36:03 +0900

    패키지

--- ch07/S05.java
+++ ch07/S05.java
@@ -1,28 +1,28 @@
-package ch07;
+// package ch07;
 
 
-public class S05 {
-    public static void main(String[] args) {
+// public class S05 {
+//     public static void main(String[] args) {
         
-        // 모든 클래스의 최상위 부모
-        Object object = new Object();
+//         // 모든 클래스의 최상위 부모
+//         Object object = new Object();
 
-        object.getClass();
-        object.hashCode();
-        object.toString();
-        object.equals(null);
-        // 등이 있다.
+//         object.getClass();
+//         object.hashCode();
+//         object.toString();
+//         object.equals(null);
+//         // 등이 있다.
 
-        // 객체는 직접 상속하지 않아도 Object를 상속 받는다.
-        String str = "홍길동";
+//         // 객체는 직접 상속하지 않아도 Object를 상속 받는다.
+//         String str = "홍길동";
 
-        str.getClass();
-        str.hashCode();
-        str.toString();
-        str.equals("김첨지");
+//         str.getClass();
+//         str.hashCode();
+//         str.toString();
+//         str.equals("김첨지");
 
-        Object str1 = new String("홍길동");
+//         Object str1 = new String("홍길동");
 
-    }
+//     }
     
-}
+// }
