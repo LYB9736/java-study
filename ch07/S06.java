fdc6b9207e155788a3e5bb9fcdb82f7402c54a3e
LYB9736 <chulfry1222@gmail.com>
2023-07-05 11:14:38 +0900

    추상클래스 인터페이스

--- ch07/S06.java
+++ ch07/S06.java
@@ -1,45 +1,45 @@
-package ch07;
+// package ch07;
 
-// 다른 패키지의 클래스를 가져올 때
-// import를 사용한다.
-import ch07.sub.Sub01;
+// // 다른 패키지의 클래스를 가져올 때
+// // import를 사용한다.
+// import ch07.sub.Sub01;
 
-public class S06 {
+// public class S06 {
 
-    public static  int first = 1557;
-    public int second = 2;
-    public static final int third = 3;
+//     public static  int first = 1557;
+//     public int second = 2;
+//     public static final int third = 3;
 
-    public void printSecond(){
-        System.out.println(second);
-    }
+//     public void printSecond(){
+//         System.out.println(second);
+//     }
 
-    public static void main(String[] args) {
-        Sub01 sub01 = new Sub01();
+//     public static void main(String[] args) {
+//         Sub01 sub01 = new Sub01();
 
-        System.out.println(sub01.getData());
+//         System.out.println(sub01.getData());
 
-        System.out.println(first); 
-        System.out.println(S06.first);
+//         System.out.println(first); 
+//         System.out.println(S06.first);
 
-        // System.out.println(second); // 가져올 수 없다.
-        // System.out.println(printSecond()); // 실행할 수 없다.
+//         // System.out.println(second); // 가져올 수 없다.
+//         // System.out.println(printSecond()); // 실행할 수 없다.
 
-        // static이 아닌 필드는 인스턴스를 생성해서 가져와야 한다.
-        // static 메소드 기준
-        S06 s06 = new S06();
-        System.out.println(s06.second);
-        s06.printSecond();
+//         // static이 아닌 필드는 인스턴스를 생성해서 가져와야 한다.
+//         // static 메소드 기준
+//         S06 s06 = new S06();
+//         System.out.println(s06.second);
+//         s06.printSecond();
 
-        S06.first = 11;
-        // final 값을 바꿀 수 없다.
-        // S06.third = 33;
+//         S06.first = 11;
+//         // final 값을 바꿀 수 없다.
+//         // S06.third = 33;
 
-        final String str = "홍길동";
-        // str = "임꺽정";
+//         final String str = "홍길동";
+//         // str = "임꺽정";
 
 
         
-    }
+//     }
     
-}
+// }
---
+++ ch07/S08Quiz.java
@@ -0,0 +1,88 @@
+package ch07;
+
+import javax.sound.midi.Soundbank;
+
+// 인터페이스 / 추상클래스 / 클래스를 이용해서
+// 스마트폰
+// 갤 / 아
+// 갤럭시 - 통화 / 문자 / 빅스비
+// 아이폰 - 통화 / 문자 / 시리
+// S23
+// 14
+
+interface 문자 {
+    void message();
+}
+
+interface 통화 {
+    void call();
+}
+
+interface 빅스비 {
+    void bixby();
+}
+
+interface 시리 {
+    void siri();
+}
+
+abstract class 스마트폰 implements 문자, 통화 {
+
+    public void message(){
+
+    }
+
+    public void call(){
+
+    }
+}
+
+class 갤럭시S23 extends 스마트폰 implements 빅스비 {
+    
+    public void message() {
+        System.out.println("문자");
+    }
+
+    public void call() {
+        System.out.println("통화");
+    }
+
+    public void bixby() {
+        System.out.println("빅스비");
+    }
+
+}
+
+class 아이폰14 extends 스마트폰 implements 시리 {
+
+
+    public void message() {
+        System.out.println("문자");
+    }
+
+    
+    public void call() {
+        System.out.println("통화");
+    }
+
+    
+    public void siri() {
+        System.out.println("시리");
+    }
+
+}
+
+public class S08Quiz {
+
+    public static void main(String[] args) {
+        
+        갤럭시S23 galaxy = new 갤럭시S23();
+
+        아이폰14 iphone = new 아이폰14();
+
+        galaxy.call();
+        iphone.siri();
+
+    }
+    
+}
